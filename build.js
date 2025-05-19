7a24d166067c79eccf0a37bbe5f4f6dc0cd776fd
eva <evagallardor@icloud.com>
2025-05-19 12:57:47 +0200

    menu y localizacion

--- build.js
+++ build.js
@@ -1,27 +1,44 @@
-import {menu} from './menu.js';
-import nunjucks from 'nunjucks';
-import fs from 'fs';
+import { menu } from './menu.js'
+import nunjucks from 'nunjucks'
+import fs from 'fs'
 
-function generateHTML(template, data) {
-    const templateContent = fs.readFileSync(template, 'utf-8');
-    const html = nunjucks.renderString(templateContent, data);
-    return html;
+function generateHTML (template, data) {
+  // Sync bloquea hasta que termina de leer template
+  const templateContent = fs.readFileSync(template, 'utf-8')
+  const html = nunjucks.renderString(templateContent, data)
+  return html
 }
-function createHTMLFile(template, data, outputFile) {
-    const html = generateHTML(template, data);
-    fs.writeFileSync(outputFile, html, 'utf-8');
-    console.log(`Archivo ${outputFile} generado con éxito.`);
+
+function createHTMLFile (template, data, outputFile) {
+  const html = generateHTML(template, data)
+  fs.writeFileSync(outputFile, html, 'utf-8')
+  console.log(`Archivo ${outputFile} generado con éxito.`)
 }
 
 if (!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
+  fs.mkdirSync('./dist')
 }
-const menuTemplate = './views/menu.njk';
-nunjucks.configure("views",{ autoescape: true });
+nunjucks.configure('views', { autoescape: true })
 
-createHTMLFile(menuTemplate, { menu }, './dist/index.html');
+const menuTemplate = './views/menu.njk'
+const locationTemplate = './views/location.njk'
 
+createHTMLFile(menuTemplate, { menu }, './dist/index.html')
+createHTMLFile(locationTemplate, { menu }, './dist/location.html')
 
-const cssFile = 'style.css';
-const outputCssFile = './dist/style.css';
-fs.copyFileSync(cssFile, outputCssFile);
+const imgsFolder = './imgs'
+const outputImgsFolder = './dist/imgs'
+if (!fs.existsSync(outputImgsFolder)) {
+  fs.mkdirSync(outputImgsFolder)
+}
+const imgFiles = fs.readdirSync(imgsFolder)
+imgFiles.forEach(file => {
+  const srcPath = `${imgsFolder}/${file}`
+  const destPath = `${outputImgsFolder}/${file}`
+  fs.copyFileSync(srcPath, destPath)
+  console.log(`Archivo ${file} copiado a ${outputImgsFolder}.`)
+})
+
+const cssFile = 'style.css'
+const outputCssFile = './dist/style.css'
+fs.copyFileSync(cssFile, outputCssFile)
--- menu.js
+++ menu.js
@@ -1,42 +1,42 @@
 export const menu = [
-    {
-        "nombre": "Entrantes",
-        "platos": [
-            { "nombre": "Ensalada César", "precio": 8.50 },
-            { "nombre": "Bruschetta", "precio": 6.00 },
-            { "nombre": "Gazpacho", "precio": 5.50 },
-            { "nombre": "Patatas Bravas", "precio": 7.00 },
-            { "nombre": "Calamares a la Romana", "precio": 9.00 }
-        ]
-    },
-    {
-        "nombre": "Platos Principales",
-        "platos": [
-            { "nombre": "Pizza Margherita", "precio": 10.00 },
-            { "nombre": "Pasta Carbonara", "precio": 12.00 },
-            { "nombre": "Paella de Mariscos", "precio": 18.00 },
-            { "nombre": "Filete de Ternera", "precio": 22.00 },
-            { "nombre": "Hamburguesa Gourmet", "precio": 15.00 }
-        ]
-    },
-    {
-        "nombre": "Postres",
-        "platos": [
-            { "nombre": "Tiramisú", "precio": 5.00 },
-            { "nombre": "Helado de Vainilla", "precio": 4.50 },
-            { "nombre": "Cheesecake", "precio": 6.00 },
-            { "nombre": "Flan Casero", "precio": 4.00 },
-            { "nombre": "Brownie con Helado", "precio": 6.50 }
-        ]
-    },
-    {
-        "nombre": "Bebidas",
-        "platos": [
-            { "nombre": "Agua Mineral", "precio": 2.00 },
-            { "nombre": "Refresco", "precio": 2.50 },
-            { "nombre": "Cerveza", "precio": 3.00 },
-            { "nombre": "Vino Tinto (copa)", "precio": 4.50 },
-            { "nombre": "Café Espresso", "precio": 2.00 }
-        ]
-    }
+  {
+    nombre: 'Entrantes',
+    platos: [
+      { nombre: 'Ensalada César', precio: 8.50 },
+      { nombre: 'Bruschetta', precio: 6.00 },
+      { nombre: 'Gazpacho', precio: 5.50 },
+      { nombre: 'Patatas Bravas', precio: 7.00 },
+      { nombre: 'Calamares a la Romana', precio: 9.00 }
+    ]
+  },
+  {
+    nombre: 'Platos Principales',
+    platos: [
+      { nombre: 'Pizza Margherita', precio: 10.00 },
+      { nombre: 'Pasta Carbonara', precio: 12.00 },
+      { nombre: 'Paella de Mariscos', precio: 18.00 },
+      { nombre: 'Filete de Ternera', precio: 22.00 },
+      { nombre: 'Hamburguesa Gourmet', precio: 15.00 }
+    ]
+  },
+  {
+    nombre: 'Postres',
+    platos: [
+      { nombre: 'Tiramisú', precio: 5.00 },
+      { nombre: 'Helado de Vainilla', precio: 4.50 },
+      { nombre: 'Cheesecake', precio: 6.00 },
+      { nombre: 'Flan Casero', precio: 4.00 },
+      { nombre: 'Brownie con Helado', precio: 6.50 }
+    ]
+  },
+  {
+    nombre: 'Bebidas',
+    platos: [
+      { nombre: 'Agua Mineral', precio: 2.00 },
+      { nombre: 'Refresco', precio: 2.50 },
+      { nombre: 'Cerveza', precio: 3.00 },
+      { nombre: 'Vino Tinto (copa)', precio: 4.50 },
+      { nombre: 'Café Espresso', precio: 2.00 }
+    ]
+  }
 ]
